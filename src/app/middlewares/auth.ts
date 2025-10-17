2d174a5784ece53cc01e88ea5dde48c717606af5
arif1101 <arifurrahmanarif223@gmail.com>
2025-10-17 17:38:06 +0600

    feat(Schedules): get all schedule  for admin& doctor

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -6,13 +6,13 @@ const auth = (...roles: string[]) => {
     return async (req: Request & {user?: any}, res: Response, next: NextFunction) => {
         try{
             const token = req.cookies.accessToken;
-
+            
             if(!token){
                 throw new Error("You are not authorized")
             }
-
+            
             const verifyUser = jwtHelper.verifyToken(token, "abcd")
-
+            
             req.user = verifyUser;
 
             if(roles.length && !roles.includes(verifyUser.role)){
